a4219f24d1d656ad6675cf36534aa4be80abbaab
Fügedi Dániel <124875643+Maxid8@users.noreply.github.com>
2026-09-08 15:40:22 +0200

    questions answered

--- queries.sql
+++ queries.sql
@@ -1,8 +1,74 @@
 -- Which categories experience the most fraud, and what is the total financial loss due to fraud per category?
--- What hours of the day and days of the week do fraudsters most often strike? Is there a detectable pattern between nighttime vs. daytime transactions?
+SELECT
+	m.category,
+	COUNT(m.category),
+	SUM(t.amt)
+FROM transactions t
+LEFT JOIN merchant m ON t.merchant_id = m.merchant_id
+WHERE t.is_fraud = 1
+GROUP BY m.category 
+ORDER BY COUNT(m.category) DESC;
+
+-- What days of the week do fraudsters most often strike? Is there a detectable pattern on weekly transactions?
+SELECT
+    d.day_name,
+    COUNT(t.is_fraud) 
+FROM transactions t 
+LEFT JOIN "date" d ON t.date_id = d.date_id
+WHERE t.is_fraud = 1
+GROUP BY d.day_name 
+ORDER BY COUNT(t.is_fraud) DESC;
+
 -- Which fraudulent transactions exceed the average transaction value for the given category?
--- Does it happen that multiple transactions are initiated from one card within a surprisingly short period of time (e.g. a few minutes)?
+SELECT
+	t.transaction_id,
+	m.merchant_name,
+	m.category,
+	t.date_id,
+	t.amt,
+	ROUND(AVG(amt) OVER (PARTITION BY m.category),2)
+FROM transactions t
+LEFT JOIN merchant m ON t.merchant_id = m.merchant_id
+WHERE t.is_fraud = 1 AND  t.amt > (SELECT AVG(amt) FROM transactions)
+ORDER BY m.category;
+
 -- What age groups and genders are most affected by fraud, and what is their average transaction size?
+SELECT
+	(FLOOR(EXTRACT(YEAR FROM AGE(c.dob))/10))*10 age_group,
+	c.gender,
+	COUNT(*) fraud_count,
+    ROUND(AVG(t.amt), 2) avg_fraud_amount
+FROM transactions t
+LEFT JOIN customer c ON t.customer_id = c.customer_id
+WHERE t.is_fraud = 1
+GROUP BY age_group, c.gender
+ORDER BY fraud_count DESC;
+
 -- How did the total financial loss from fraud accumulate day by day over time?
+WITH daily_loss AS (
+    SELECT
+        date_id AS day,
+        SUM(amt) AS daily_total
+    FROM transactions
+    WHERE is_fraud = 1
+    GROUP BY date_id
+)
+SELECT
+    "day",
+    daily_total,
+    SUM(daily_total) OVER (ORDER BY "day") cumulative_loss
+FROM daily_loss
+ORDER BY "day";
+
 -- Who are the merchants whose number of transactions reaches a minimum value (e.g. at least 50 transactions), but whose fraud rate is exceptionally high (>5%)?
--- Is there a correlation between the discrepancy between the cardholder's residence and the merchant's geographic location (lat/long distance) and the likelihood of fraud?
+SELECT
+    m.merchant_name,
+    SUM(t.is_fraud) AS sum_of_merch_fraud,
+    COUNT(t.transaction_id) AS sum_of_transactions,
+    ROUND(SUM(t.is_fraud)::decimal / COUNT(*), 4) AS fraud_rate
+FROM transactions t
+LEFT JOIN merchant m ON t.merchant_id = m.merchant_id
+GROUP BY m.merchant_name
+HAVING COUNT(t.transaction_id) >= 50
+   AND SUM(t.is_fraud)::decimal / COUNT(*) > 0.05
+ORDER BY fraud_rate DESC;
